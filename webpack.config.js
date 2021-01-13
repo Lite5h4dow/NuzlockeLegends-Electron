3727e3ed362226944dc0a0b15bc2f563f8f9e078
Lamar Daughma <lite5h4dow@gmail.com>
2021-01-13 01:11:46 +0000

    update webpack config syntax

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     rules: [
       { test: /\.(ts|js)x?$/, include: SRC_DIR, loader: "babel-loader" },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(jpg|png|gif|jpeg|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+      { test: /\.(jpg|png|gif|jpeg|woff|woff2|eot|ttf|svg)$/, use: 'url-loader?limit=100000' }
     ]
   },
   plugins: [
